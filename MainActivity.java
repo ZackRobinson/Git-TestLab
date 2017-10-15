ebcb2c24fa05c5393f06ac166b600b465b113462
Zack Robinson <zwrprofessional@gmail.com>
2017-10-15 19:38:45 -0400

    Added comment to the main class to test merges

--- MainActivity.java
+++ MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Testing the differencer in Git
+        //Test of testing-merge branch
     }
 }
